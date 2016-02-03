677e07f0f9f653dec63d72e1fcaf6fd3ca3c7925
Thom <thomascjohnson@gmail.com>
2016-02-03 14:25:30 -0300

    Update github_sourcer.R
    Forgot to store the repo in the class

--- R/github_sourcer.R
+++ R/github_sourcer.R
@@ -19,12 +19,13 @@
 CIRepo <- R6::R6Class("CIRepo",
   public = list(
     github_token = NULL,
-    
+    repo = NULL,
     initialize = function(pat, repo = "vs-indicators-calc-private") {
       self$github_token <- pat
+      self$repo = repo
     },
     sourceRCode = function(repo_path) {
-      github_url <- paste0(gsub("REPO", repo, private$RCode.url), repo_path)
+      github_url <- paste0(gsub("REPO", self$repo, private$RCode.url), repo_path)
       print(github_url)
       github_response <- httr::GET(github_url,
                                    httr::add_headers("Authorization" =
@@ -40,7 +41,7 @@ CIRepo <- R6::R6Class("CIRepo",
       }
     },
     parseRCode = function(repo_path) {
-      github_url <- paste0(gsub("REPO", repo, private$RCode.url), repo_path)
+      github_url <- paste0(gsub("REPO", self$repo, private$RCode.url), repo_path)
       print(github_url)
       github_response <- httr::GET(github_url,
                                    httr::add_headers("Authorization" =
